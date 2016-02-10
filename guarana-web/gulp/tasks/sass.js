4582e699e3c5322fafd05ccf89bd00325700bd22
Caleb Briancesco <jcaleb4@hotmail.com>
2016-02-10 10:28:31 -0600

    changing to sass

--- guarana-web/gulp/tasks/sass.js
+++ guarana-web/gulp/tasks/sass.js
@@ -1,14 +1,10 @@
-'use strict';
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    config = require('../../config.json');
+
+// Get one .styl file and render 
 gulp.task('sass', function () {
-  return gulp.src('./src/styles/**/*.scss')
+    gulp.src(config.paths.dev.css.pages + '*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+    .pipe(gulp.dest(config.paths.prod.css.root));
 });
